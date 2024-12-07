9c890f5ee9ed0f1cfc0e4e2b2fe00cc4ad798a28
Kqliber <kqliber@gmail.com>
2024-12-07 15:06:51 +0000

    javafx

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
 plugins {
     java
+    application
+    id("org.openjfx.javafxplugin") version "0.1.0"
+}
+
+javafx {
+    version = "23.0.1"
+    modules = listOf("javafx.controls")
+}
+
+application {
+    mainClass = "com.github.itdaag.memflow.Main"
 }
 
 repositories {
